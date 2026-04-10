39101b35a3a8cea4be044d517587074b57cdb22e
WANZIRE VINYWA Guy <24wv439si@udbl.ac.cd>
2026-04-10 19:06:41 +0000

    Finalisation du Portfolio avec Script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+// Vérifier si un thème est déjà enregistré dans le navigateur
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+// Fonction pour changer de thème
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); // Garde en mémoire le choix
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
